a238ee9ad4e58811ab9443e95d570db727184e4f
Haszon Kevin <khaszon@gmail.com>
2022-12-07 00:29:05 +0100

    merge

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});*/
+
+Route::group(['prefix' => '/users'], function () {
+    Route::get("/", [UserController::class, "index"])->name("getUsers");
+    Route::post("/create", [UserController::class, "create"])->name("createClass");
+    Route::put("/{user}", [UserController::class, "update"])->name("updateClass");
+    Route::delete("/{user}", [UserController::class, "delete"])->name("deleteClass");
 });
