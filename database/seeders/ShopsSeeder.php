6a2e6ca61df8553b8477a5900bc2279272b6c8b8
Farkas Péter <farkasp@ntszki.hu>
2023-01-24 19:31:30 +0100

    gitflow-feature-stash: MoreFakeData

--- database/seeders/ShopsSeeder.php
+++ database/seeders/ShopsSeeder.php
@@ -47,6 +47,70 @@ class ShopsSeeder extends Seeder
                 'owner' => "Bestbyte műszaki",
                 'postal_code' => 9730
             ],
+            [
+                'id' => 5,
+                'name' => "TESCO expressz",
+                'shop_type_id' => "3",
+                'address' => "IV. László király u. 39",
+                'owner' => "Tesco PLC",
+                'postal_code' => 9400
+            ],
+            [
+                'id' => 6,
+                'name' => "TESCO expressz",
+                'shop_type_id' => "3",
+                'address' => "Király J. u 3",
+                'owner' => "Tesco PLC",
+                'postal_code' => 9400
+            ],
+            [
+                'id' => 7,
+                'name' => "TESCO Hipermarket",
+                'shop_type_id' => "2",
+                'address' => "Ipar krt. 30",
+                'owner' => "Tesco PLC",
+                'postal_code' => 9400
+            ],
+            [
+                'id' => 8,
+                'name' => "Tesco Expressz",
+                'shop_type_id' => "3",
+                'address' => "Hátsókapu 10",
+                'owner' => "Tesco PLC",
+                'postal_code' => 9400
+            ],
+            [
+                'id' => 9,
+                'name' => "TESCO expressz",
+                'shop_type_id' => "3",
+                'address' => "József A. u 1",
+                'owner' => "Tesco PLC",
+                'postal_code' => 9400
+            ],
+            [
+                'id' => 10,
+                'name' => "TESCO expressz",
+                'shop_type_id' => "3",
+                'address' => "Lackner Kristóf u. 11",
+                'owner' => "Tesco PLC",
+                'postal_code' => 9400
+            ],
+            [
+                'id' => 11,
+                'name' => "TESCO expressz",
+                'shop_type_id' => "3",
+                'address' => "Végfordulat 9",
+                'owner' => " Peter Gazik",
+                'postal_code' => 9400
+            ],
+            [
+                'id' => 12,
+                'name' => "Yettel",
+                'shop_type_id' => "3",
+                'address' => " Ipar krt. 30",
+                'owner' => "Tesco PLC",
+                'postal_code' => 9400
+            ],
         ];
         foreach ($data as $item) {
             $shop = Shop::firstOrNew([
