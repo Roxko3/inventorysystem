6b7578e4bbc8a0e020ef16a064b3040ba8b97ae3
WhiteWolf <khaszon@gmail.com>
2023-02-08 18:04:55 +0100

    Search and Order by

---
+++ app/Http/Requests/OrderRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator as Validator;
+use Illuminate\Validation\ValidationException;
+
+class OrderRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'column' => "nullable",
+            'order' => "nullable",
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw (new ValidationException($validator, response()->json($validator->errors(), 422)));
+    }
+}
