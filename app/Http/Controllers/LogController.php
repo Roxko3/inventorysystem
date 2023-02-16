1b217b341ba85f264609b0cafb0de2b429a39f36
WhiteWolf <khaszon@gmail.com>
2023-02-16 23:51:46 +0100

    Log search

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\LogRequest;
+use App\Http\Requests\LogSearchRequest;
 use App\Models\Log;
 use App\Models\Shop;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Gate;
 
 class LogController extends Controller
@@ -19,5 +20,40 @@ class LogController extends Controller
         }
         $logs = Log::with("user")->where('shop_id', $shop->id)->get();
         return response()->json($logs);
+    }
+
+    public function searchLogs(LogSearchRequest $request, Shop $shop)
+    {
+        if ($request->get("column") == "name") {
+            $storage = DB::table('logs')
+                ->select('logs.*')
+                ->join('users', 'logs.user_id', '=', 'users.id')
+                ->where('logs.shop_id', $shop->id)
+                ->where(function ($query) use ($request) {
+                    $query->where('users.name', 'like', '%' . $request->get("searchString") . '%')
+                        ->orWhere('logs.description', 'like', '%' . $request->get("searchString") . '%')
+                        ->orWhere('logs.date', 'like', '%' . $request->get("searchString") . '%');
+                })
+                ->orderBy('users.name', $request->get("order") == "desc" ? "desc" : "asc")
+                ->paginate(20);
+        } else {
+            if ($request->get("column") == null) {
+                $ordercolumn = "logs.id";
+            } else {
+                $ordercolumn = "logs." . $request->get("column");
+            }
+            $storage = DB::table('logs')
+                ->select('logs.*')
+                ->join('users', 'logs.user_id', '=', 'users.id')
+                ->where('logs.shop_id', $shop->id)
+                ->where(function ($query) use ($request) {
+                    $query->where('users.name', 'like', '%' . $request->get("searchString") . '%')
+                        ->orWhere('logs.description', 'like', '%' . $request->get("searchString") . '%')
+                        ->orWhere('logs.date', 'like', '%' . $request->get("searchString") . '%');
+                })
+                ->orderBy($ordercolumn, $request->get("order") == "desc" ? "desc" : "asc")
+                ->paginate(20);
+        }
+        return response()->json($storage);
     }
 }
---
+++ app/Http/Requests/LogSearchRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Rules\CheckColumnRule;
+use App\Rules\LogSearchColumnRule;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\ValidationException;
+
+class LogSearchRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'column' => ["nullable", new LogSearchColumnRule],
+            'order' => "nullable",
+            'searchString' => "nullable"
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw (new ValidationException($validator, response()->json($validator->errors(), 422)));
+    }
+}
